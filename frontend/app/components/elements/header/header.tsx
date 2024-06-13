d38e7789e6bcfa236212cf1ac96d6cc337fe40a9
miruwan <agou.ar1209@gmail.com>
2024-06-13 15:18:21 +0900

    feat: ヘッダーに通知アイコンを表示

--- frontend/app/components/elements/header/header.tsx
+++ frontend/app/components/elements/header/header.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import styled from "@mui/material/styles/styled";
+
+// 通知アイコンのスタイル
+const NotifyIcon = styled(CircleNotificationsIcon)({
+  fontSize: 35,
+});
 
 // ヘッダータイトルのスタイル
 const HeaderTitle = styled(Typography)({
@@ -18,6 +25,7 @@ export const Header = () => {
     <>
       <AppBar component="nav">
         <Toolbar>
+          <NotifyIcon />
           <HeaderTitle variant="h5">Rakuten Price Notify</HeaderTitle>
         </Toolbar>
       </AppBar>
