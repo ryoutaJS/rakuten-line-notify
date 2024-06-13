35f8406fa7e2392d7a2d31c709aff408d77fa861
miruwan <agou.ar1209@gmail.com>
2024-06-13 15:16:40 +0900

    feat: ヘッダータイトルにスタイルを適応
     - MUIのstyledでヘッダータイトルにスタイルを適応しました。
     - フォントをmonospaceに設定
     - 太文字に設定
     - 左側に余白を設定
     - 文字の間隔を設定

--- frontend/app/components/elements/header/header.tsx
+++ frontend/app/components/elements/header/header.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import styled from "@mui/material/styles/styled";
+
+// ヘッダータイトルのスタイル
+const HeaderTitle = styled(Typography)({
+  fontWeight: "bold",
+  fontFamily: "monospace",
+  marginLeft: ".5rem",
+  letterSpacing: ".15rem",
+});
 
 export const Header = () => {
   return (
     <>
       <AppBar component="nav">
         <Toolbar>
-          <Typography variant="h5">楽天価格通知アプリ</Typography>
+          <HeaderTitle variant="h5">Rakuten Price Notify</HeaderTitle>
         </Toolbar>
       </AppBar>
     </>
