1bc799684de821446b748e2ea6469b6dcf2d820d
miruwan <agou.ar1209@gmail.com>
2024-06-13 15:29:47 +0900

    feat: ヘッダー内の要素を中央揃えに設定

--- frontend/app/components/elements/header/header.tsx
+++ frontend/app/components/elements/header/header.tsx
@@ -3,6 +3,7 @@
 import CircleNotificationsIcon from "@mui/icons-material/CircleNotifications";
 
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import styled from "@mui/material/styles/styled";
@@ -20,13 +21,23 @@ const HeaderTitle = styled(Typography)({
   letterSpacing: ".15rem",
 });
 
+// ヘッダー内の要素を中央揃え
+const CenterBox = styled(Box)({
+  flexGrow: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
 export const Header = () => {
   return (
     <>
       <AppBar component="nav">
         <Toolbar>
-          <NotifyIcon />
-          <HeaderTitle variant="h5">Rakuten Price Notify</HeaderTitle>
+          <CenterBox>
+            <NotifyIcon />
+            <HeaderTitle variant="h5">Rakuten Price Notify</HeaderTitle>
+          </CenterBox>
         </Toolbar>
       </AppBar>
     </>
