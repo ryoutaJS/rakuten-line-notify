82867174e468d45929eb4fc732e6e2489833751e
miruwan <agou.ar1209@gmail.com>
2024-06-18 00:14:12 +0900

    feat: レスポンシブ対応

--- frontend/app/components/elements/ItemsCard/ItemsCard.tsx
+++ frontend/app/components/elements/ItemsCard/ItemsCard.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
 import { Title } from "../Title/Title";
@@ -37,33 +39,39 @@ export const ItemsCard = async () => {
 
   return (
     <>
-      <div style={{ marginTop: "2%", marginBottom: "1%" }}>
-        <Title />
-      </div>
-      {itemsData.map((data) => (
-        <Card key={data.genreId}>
-          <Link href={data.itemUrl} target="_blank">
-            <CardMedia
-              component="img"
-              height="250"
-              image={data.imageUrl}
-              style={{ objectFit: "contain" }}
-            />
-          </Link>
-          <CardContent>
-            <Typography
-              variant="body2"
-              title={data.itemName}
-              sx={itemNameStyle}
-            >
-              {data.itemName}
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {formatCurrency(data.itemPrice)}
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
+      <Container maxWidth="xl">
+        <div style={{ marginTop: "2%", marginBottom: "1%" }}>
+          <Title />
+        </div>
+        <Grid container rowSpacing={2} columnSpacing={2}>
+          {itemsData.map((data) => (
+            <Grid key={data.genreId} item xs={12} sm={6} md={3} lg={2}>
+              <Card key={data.genreId}>
+                <Link href={data.itemUrl} target="_blank">
+                  <CardMedia
+                    component="img"
+                    height="250"
+                    image={data.imageUrl}
+                    style={{ objectFit: "contain" }}
+                  />
+                </Link>
+                <CardContent>
+                  <Typography
+                    variant="body2"
+                    title={data.itemName}
+                    sx={itemNameStyle}
+                  >
+                    {data.itemName}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    {formatCurrency(data.itemPrice)}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 };
