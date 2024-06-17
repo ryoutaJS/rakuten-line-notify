13869485a981201cd4f770ddafe1cf05660d7620
miruwan <agou.ar1209@gmail.com>
2024-06-17 19:30:47 +0900

    rename: ヘッダーコンポーネントのファイル名を変更
     - 他のコンポーネントファイルに合わせて、ファイル名をパスカルケースに変更
     - ファイル名の変更に伴い、ホーム画面のimport文を修正

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/elements/header/header";
+import { Header } from "./components/elements/Header/Header";
 import { ItemsCard } from "./components/elements/ItemsCard/ItemsCard";
 
 export default function Home() {
