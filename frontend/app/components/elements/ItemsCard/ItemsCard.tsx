11f5e06c8c8b453cd4d771769e782126f63599cd
miruwan <agou.ar1209@gmail.com>
2024-06-17 23:26:23 +0900

    feat: 商品画像を表示
     - 商品画像をクリックしたら、楽天の商品ページを別タブで開くように実装しました。

--- frontend/app/components/elements/ItemsCard/ItemsCard.tsx
+++ frontend/app/components/elements/ItemsCard/ItemsCard.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+
 import { Title } from "../Title/Title";
 import { useFetchItemsData } from "./ItemsCard.hooks";
 
@@ -10,6 +15,18 @@ export const ItemsCard = async () => {
       <div style={{ marginTop: "2%", marginBottom: "1%" }}>
         <Title />
       </div>
+      {itemsData.map((data) => (
+        <Card key={data.genreId}>
+          <Link href={data.itemUrl} target="_blank">
+            <CardMedia
+              component="img"
+              height="250"
+              image={data.imageUrl}
+              style={{ objectFit: "contain" }}
+            />
+          </Link>
+        </Card>
+      ))}
     </>
   );
 };
