4bf890ff4e2f1f1e0d33a1cf90cab66dd0eb8ccd
miruwan <agou.ar1209@gmail.com>
2024-06-30 18:18:47 +0900

    feat: LINE通知を送る関数を作成
     - get_api_key()でSecrets ManagerからLINE Notify API のアクセストークンを取得する
     - 自分のLINEアカウントに楽天商品のお買い得情報の通知を送る

---
+++ backend/price_line_notify/send_line_notify.py
@@ -0,0 +1,22 @@
+import requests
+from get_api_key import get_api_key
+
+
+def send_line_notify(discount: int, item_url: str) -> None:
+    """
+    お買い得情報のLINE通知を送る
+    discount: int - 商品の割引額
+    item_url: str - 商品ページのURL
+    """
+
+    line_notify_api = "https://notify-api.line.me/api/notify"
+    line_notify_token = get_api_key("LINE_NOTIFY_TOKEN")
+
+    headers = {"Authorization": f"Bearer {line_notify_token}"}
+    data = {"message": f"以下の商品が{discount}円お買い得になりました！{item_url}"}
+
+    requests.post(
+        url=line_notify_api,
+        headers=headers,
+        data=data,
+    )
