29b6a212c295b142dace92ce50b0c504f53676e1
miruwan <agou.ar1209@gmail.com>
2024-06-30 17:04:21 +0900

    feat: 楽天APIから商品データを取得する関数を作成
     - get_api_key()でSecrets Managerから楽天APIのアクセスキーを取得する
     - itemCodeを条件に楽天APIから商品データを取得する

---
+++ backend/price_line_notify/fetch_rakuten_item.py
@@ -0,0 +1,24 @@
+import requests
+from get_api_key import get_api_key
+
+
+def fetch_rakuten_item(item_code: str) -> dict:
+    """
+    楽天APIから特定の商品データを取得
+    item_code: str - 楽天APIの商品コード
+    return: dict   - 商品情報データ
+    """
+
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
+    params = {
+        "applicationId": get_api_key("RAKUTEN_API_KEY"),
+        "itemCode": item_code,
+    }
+    
+    response: dict = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['Items'][0]['Item']
+    else:
+        response.raise_for_status()
