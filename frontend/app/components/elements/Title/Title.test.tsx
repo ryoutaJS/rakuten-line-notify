2a9f46ce9a05f97c8a239780c48f3aa7bc4aca96
miruwan <agou.ar1209@gmail.com>
2024-08-05 22:06:25 +0900

    feat: Titleコンポーネントの単体テストを実装
     - Vitestを使用したTitleコンポーネントのユニットテストの実装
     - Titleコンポーネントの正しいレンダリングをテスト

---
+++ frontend/app/components/elements/Title/Title.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect, beforeEach } from "vitest";
+import { Title } from "./Title";
+
+describe("Title component", () => {
+  beforeEach(() => {
+    render(<Title />);
+  });
+
+  test("タイトルが表示されてるか", () => {
+    const title = screen.getByText("ほしいものリスト");
+    expect(title).toBeInTheDocument();
+  });
+
+  test("infoアイコンにツールチップが設定されているか", () => {
+    const infoIcon = screen.getByRole("button");
+    expect(infoIcon).toBeInTheDocument();
+    expect(infoIcon).toHaveAttribute(
+      "aria-label",
+      "リストに表示されている商品の価格がセールなどで安くなったらLINE通知が届きます。",
+    );
+  });
+});
