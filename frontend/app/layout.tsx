61f92e1d642f67685e202dd1997e8ea4016cf816
miruwan <agou.ar1209@gmail.com>
2024-09-29 17:55:31 +0900

    feat: html head tag

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,7 +9,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
+      <head>
+        <title>楽天価格通知アプリ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/notify.jpg" />
+      </head>
       <body style={{ background: '#f5f5f5' }} className={inter.className}>
         {children}
       </body>
